fcad479621d1de4013676a41cbcb082595d4b25b
admin <admin@USCS-Mac277.local>
2020-02-24 20:25:36 -0800

    SignUp fields added

--- Frontend/src/components/Account/SignUp.js
+++ Frontend/src/components/Account/SignUp.js
@@ -29,8 +29,41 @@ class SignUp extends Component{
                 <div class="right-half">
                 <h1 class="style_heading">Let's find your next job</h1>
 
-                <div class="style__text">Join Handshake's community of students, <br></br>schools, and employers to find internships,<br></br> land a job, and more.</div>
-                               
+                <div class="style__text">Join Handshake's community of students, <br></br>schools, and employers to find internships,<br></br> land a job, and more.
+                </div>
+
+                <form action='action_page.php' style={{border:'1px' , color: 'solid #ccc'}}>
+                    <div class="containerNew">
+                        
+                        {/* <hr /> */}
+
+                        <label for="firstName"><b>First Name</b></label><br />
+                        <input type="text" placeholder="First Name" name="firstName" required /> <br />
+
+                        <label for="lastName"><b>Last Name</b></label> <br />
+                        <input type="text" placeholder="Last Name" name="lastName" required /> <br />
+
+                        <label for="sjsuID"><b>SJSU ID</b></label><br />
+                        <input type="number" placeholder="Enter SJSU ID" name="sjsuID" required /><br />
+
+                        <label for="major"><b>Major</b></label><br />
+                        <input type="text" placeholder="Major" name="major" required /><br />
+
+                        <label for="email"><b>Email</b></label><br />
+                        <input type="text" placeholder="Enter Email" name="email" required /><br />
+
+                        <label for="psw"><b>Password</b></label><br />
+                        <input type="password" placeholder="Enter Password" name="psw" required /><br />
+
+                        <label for="psw-repeat"><b>Repeat Password</b></label><br />
+                        <input type="password" placeholder="Repeat Password" name="psw-repeat" required />
+
+                        <div class="clearfix">
+                        <button type="button" class="cancelbtn">Cancel</button>
+                        <button type="submit" class="signupbtn">Sign Up</button>
+                        </div>
+                    </div>
+                </form>                               
                 </div>
             </div>
         )
